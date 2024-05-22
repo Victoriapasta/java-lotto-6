f85fe73037d75fe0c46df6c4a00f59444fa21987
Victoriapasta <timel1109@naver.com>
2024-05-22 12:36:15 +0900

    feat: 로또의 상금 계산 클래스 생성

---
+++ src/main/java/lotto/domain/PrizeRank.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public enum PrizeRank {
+
+    FIRST(6, false),
+    SECOND(5, true),
+    THIRD(5, false),
+    FOURTH(4, false),
+    FIFTH(3, false),
+    OTHERS(0, false);
+
+    private final Integer matchedNumber;
+    private final boolean matchedBonusNumber;
+
+    PrizeRank(Integer matchedNumber, boolean matchedBonusNumber) {
+        this.matchedNumber = matchedNumber;
+        this.matchedBonusNumber = matchedBonusNumber;
+    }
+
+    public PrizeRank setRank(Integer matchedNumber, boolean matchedBonusNumber) {
+        for (PrizeRank prizeRank : PrizeRank.values()) {
+            if (prizeRank.matchedNumber.equals(matchedNumber) && prizeRank.matchedBonusNumber == matchedBonusNumber) {
+                return prizeRank;
+            }
+        }
+        return OTHERS;
+    }
+}
